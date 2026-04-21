7da227808153b5af3f98da123c48e9d601a36b96
Abhay <abhaypvt2903@gmail.com>
2026-04-21 17:08:45 +0530

    Add solved printMazePaths using recursion

---
+++ Recursion/PrintMazePaths.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class PrintMazePaths {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        printMazePaths(1,1,n,m,"");
+
+    }
+
+    public static void printMazePaths(int sr , int sc, int dr, int dc , String psf){
+        if(sr>dr || sc> dc){
+            return;
+        }
+        if(sr==dr && sc==dc){
+            System.out.println(psf);
+            return;
+        }
+
+        printMazePaths(sr, sc+1,dr,dc,psf + "h");
+        printMazePaths(sr+1 , sc , dr, dc ,psf + "v");
+    }
+}
