3b3fc4640c21e2ffc2ab8cbafdc214b81f1b706d
Abhay <abhaypvt2903@gmail.com>
2026-04-22 19:28:04 +0530

    Add solution of printPermutations using recursion

---
+++ Recursion/PrintPermutations.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrintPermutations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        printPermutations(str,"");
+    }
+    public static void printPermutations(String ques, String asf){
+        if(ques.length() == 0){
+            System.out.println(asf);
+            return;
+        }
+
+        for(int i =0;i<ques.length();i++){
+            char ch = ques.charAt(i);
+            String qlpart = ques.substring(0,i);
+            String qrpart = ques.substring(i+1);
+            String roq = qlpart + qrpart;
+            printPermutations(roq, asf + ch);
+        }
+    }
+}
