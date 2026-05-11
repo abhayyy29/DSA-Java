5b5b564558e2aa86cac50afd03ddf9a75fe284db
Abhay <abhaypvt2903@gmail.com>
2026-05-11 18:58:49 +0530

    Add solution of Leaders in Array

---
+++ Arrays/LeadersInArray.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LeadersInArray {
+    public List<Integer> leaders(int[] nums){
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        int maxRight = nums[n-1];
+        ans.add(maxRight);
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]>maxRight){
+            maxRight = nums[i];
+            ans.add(nums[i]);
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {10,22,12,3,0,6};
+
+        LeadersInArray ab = new LeadersInArray();
+        List<Integer> result = ab.leaders(nums);
+        System.out.println(result);
+    }
+}
