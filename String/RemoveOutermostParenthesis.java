100bc78c9af791463a7c03d9a4d32fe2b2be4cdf
abhayyy29 <abhaypvt2903@gmail.com>
2026-07-21 13:52:43 +0530

    Add soluton of RemoveOutermostParenthesis problem

---
+++ String/RemoveOutermostParenthesis.java
@@ -0,0 +1,27 @@
+package String;
+public class RemoveOutermostParenthesis {
+    public String removeOutermost(String s){
+
+        int count =0;
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count != 0){
+                ans.append(s.charAt(i));
+            }
+
+            if(s.charAt(i) == '('){
+                count++;
+            }
+        }
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String s = "((()))";
+        RemoveOutermostParenthesis ab = new RemoveOutermostParenthesis();
+        System.out.println(ab.removeOutermost(s));
+    }
+}
