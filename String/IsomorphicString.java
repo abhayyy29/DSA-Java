73897c9c97b2a49d8056e77172e44ee7a7d0ba01
abhayyy29 <abhaypvt2903@gmail.com>
2026-07-22 16:16:38 +0530

    Add solution of Isomorphic String

---
+++ String/IsomorphicString.java
@@ -0,0 +1,42 @@
+package String;
+
+import java.util.HashMap;
+
+public class IsomorphicString {
+    public boolean isIsomorphic(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character,Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if(mapST.containsKey(ch1)){
+                if(mapST.get(ch1) != ch2){
+                    return false;
+                }else{
+                    mapST.put(ch1, ch2);
+                }
+
+                if(mapTS.containsKey(ch2)){
+                    if(mapTS.get(ch2) != ch1){
+                        return false;
+                    }else{
+                        mapTS.put(ch2, ch1);
+                    }
+                }   
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "add";
+        String t = "egg";
+        IsomorphicString ab = new IsomorphicString();
+        System.out.println(ab.isIsomorphic(s, t));
+    }
+}
