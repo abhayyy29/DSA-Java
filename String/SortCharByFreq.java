022a09620575b5043f62113fb4b3b133dfc7f926
abhayyy29 <abhaypvt2903@gmail.com>
2026-07-24 23:26:26 +0530

    Add solution of sort char by freq

---
+++ String/SortCharByFreq.java
@@ -0,0 +1,34 @@
+package String;
+import java.util.*;
+
+class SortCharByFreq {
+
+    public List<Character> frequencySort(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        List<Character> list = new ArrayList<>(map.keySet());
+
+       
+        Collections.sort(list, (a, b) -> {
+
+            if (map.get(a).equals(map.get(b))) {
+                return a - b;
+            }
+
+            return map.get(b) - map.get(a);
+        });
+
+        return list;
+    }
+    public static void main(String[] args) {
+        String s = "tree";
+        SortCharByFreq ab = new SortCharByFreq();
+        System.out.println(ab.frequencySort(s));
+    }
+}
