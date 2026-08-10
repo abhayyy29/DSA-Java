7052738ac67fa9ffaf913532b43be21a261cc575
abhayyy29 <abhaypvt2903@gmail.com>
2026-08-10 13:31:09 +0530

    Add solution for the longest happy prefix problem

---
+++ String/LongestHappyPrefix.java
@@ -0,0 +1,37 @@
+package String;
+
+import java.util.List;
+
+class LongestHappyPrefix {
+    public String search(String s){
+        int n = s.length();
+        int[] lps = new int[n];
+
+        int len =0;
+        int i=1;
+
+        while(i<n){
+            if(s.charAt(i) == s.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len != 0){
+                    len = lps[len-1];
+                }else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        int length = lps[n-1];
+        return s.substring(0,length);
+    }
+    public static void main(String[] args) {
+        String s = "ababab";
+        LongestHappyPrefix ab = new LongestHappyPrefix();
+        String result = ab.search(s);
+        System.out.println(result);
+        
+    }
+}
