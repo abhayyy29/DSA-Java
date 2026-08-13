50d21045e3eb3f57efc2e3e3e31a231b11d611a1
abhayyy29 <abhaypvt2903@gmail.com>
2026-08-13 12:19:24 +0530

    Add solution of common elements II problem

---
+++ Hashing/GetCommonElementII.java
@@ -0,0 +1,31 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class GetCommonElementII {
+    public static void main(String[] args) {
+        int[] a1 = {1,1,2,2,2,3,5};
+        int[] a2 = {1,1,1,2,2,4,5};
+
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        for(int val : a1){
+            if(hm.containsKey(val)){
+            int of = hm.get(val);
+            int nf = of + 1;
+            hm.put(val,nf);
+        }else{
+            hm.put(val,1);
+        }
+    }
+
+    for(int val : a2){
+        if(hm.containsKey(val) && hm.get(val) > 0){
+            System.out.print(val);
+            int of = hm.get(val);
+            int nf = of -1;
+            hm.put(val, nf);
+        }
+    }
+    }
+}
