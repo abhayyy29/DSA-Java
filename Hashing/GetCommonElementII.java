92ce5945c24b57bbf6a604fe314d4b9e1164b54b
abhayyy29 <abhaypvt2903@gmail.com>
2026-08-14 13:35:00 +0530

    Add solution of longest consecutive seq problem in java

--- Hashing/GetCommonElementII.java
+++ Hashing/GetCommonElementII.java
@@ -4,28 +4,31 @@ import java.util.HashMap;
 
 public class GetCommonElementII {
     public static void main(String[] args) {
-        int[] a1 = {1,1,2,2,2,3,5};
-        int[] a2 = {1,1,1,2,2,4,5};
+        int[] a1 = {1,1,1,2,2,4,5};
+        int[] a2 = {1,1,2,3,5};
 
         HashMap<Integer,Integer> hm = new HashMap<>();
 
         for(int val : a1){
             if(hm.containsKey(val)){
-            int of = hm.get(val);
-            int nf = of + 1;
-            hm.put(val,nf);
-        }else{
-            hm.put(val,1);
-        }
-    }
 
-    for(int val : a2){
-        if(hm.containsKey(val) && hm.get(val) > 0){
-            System.out.print(val);
-            int of = hm.get(val);
-            int nf = of -1;
-            hm.put(val, nf);
+                int of = hm.get(val);
+                int nf = of + 1;
+                hm.put(val, nf);
+            }else{
+                hm.put(val,1);
+            }
         }
-    }
+
+        for(int val: a2){
+            if(hm.containsKey(val)){
+                System.out.println(val);
+                int of = hm.get(val);
+                int nf = of -1;
+                hm.put(val,nf);
+            }
+        }
+
+
     }
 }
---
+++ Hashing/LongestConsecutiveSeq.java
@@ -0,0 +1,42 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class LongestConsecutiveSeq {
+    public static void main(String[] args) {
+       int[] arr = {10,5,9,1,11,8,6,15,3,12,2};
+
+       HashMap<Integer,Boolean> map = new HashMap<>();
+
+       for(int val : arr){
+        map.put(val,true);
+       }
+
+       for(int val: arr){
+        if(map.containsKey(val-1)){
+        map.put(val,false);
+       }
+    }
+
+        int ml = 0;
+        int msp = 0;
+    for(int val: arr){
+        if(map.get(val) == true){
+            int tl = 1;
+            int tsp = val;
+
+            while(map.containsKey(tsp + tl)){
+                tl++;
+            }
+
+            if(tl > ml){
+                msp = tsp;
+                ml = tl;
+            }
+        }
+    }
+    for(int i = 0; i< ml; i++){
+        System.out.println(msp + i);
+    }
+    }
+}
