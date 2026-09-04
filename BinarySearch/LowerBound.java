288eaf32498ab8090e9edd394f4843f9e863cc29
abhayyy29 <abhaypvt2903@gmail.com>
2026-09-04 15:04:46 +0530

    Add changes in Lower Bound problem

--- BinarySearch/LowerBound.java
+++ BinarySearch/LowerBound.java
@@ -8,7 +8,19 @@ public class LowerBound {
 
         while(low <= high){
             int mid = (low + high)/2;
-            if(arr[mid] )
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
         }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3};
+        int x = 2;
+        LowerBound ab = new LowerBound();
+        System.out.println(ab.search(arr, x));
     }
 }
